834a1648ea838736f5d78741cb1db6549b1a9747
NimmyVipin <nimmyvipin25@gmail.com>
2016-11-04 18:06:18 +0530

    add feature display header tags

--- plink.rb
+++ plink.rb
@@ -1,13 +1,20 @@
 require 'open-uri'
 require 'nokogiri'
 
-
-
 page="https://redpanthers.co"
 doc = Nokogiri::HTML(open(page))
 title = doc.css('title').text
-
 puts "Title: #{title}"
 
+h1 = doc.css('h1').text
+puts "h1: #{h1}"
 
+h2 = doc.css('h2').text
+puts "h2: #{h2}"
+
+h3 = doc.css('h3').text
+puts "h3: #{h3}"
+
+h4 = doc.css('h4').text
+puts "h4: #{h4}"
 
