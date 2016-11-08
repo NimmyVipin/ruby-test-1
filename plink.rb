8314119dd12c96e082df161cf41c04ea7531a836
NimmyVipin <nimmyvipin25@gmail.com>
2016-11-08 09:34:04 +0530

    modify plink.rb

--- plink.rb
+++ plink.rb
@@ -1,17 +1,17 @@
+#!/usr/bin/env ruby
 require 'open-uri'
 require 'nokogiri'
 require 'optparse'
 
+
 options = {}
 op= OptionParser.new do|opts|
-  opts.on('-n', '--site site', 'site') do |site|
+  opts.on('-n', '--website site', 'site') do |site|
    options[:site] = site
   end   
  end
 op.parse!
 
-print 'Enter the site:'
-options[:site] = gets.chomp
 
 url =  options[:site]
 doc = Nokogiri::HTML(open(url))
@@ -20,7 +20,7 @@ ary=['title','h1','h2','h3','h4']
 ary.each do |r|
  val = doc.css(r).text 
  puts "#{r}:"
- print "#{val}"
+ puts "#{val}"
 end
 
 
